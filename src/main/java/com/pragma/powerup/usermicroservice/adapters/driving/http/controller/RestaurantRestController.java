e026fc996987a6abfe8ce805c1acbc2eddb95041
CharlSK8 <charlinson2013@gmail.com>
2023-06-01 21:22:44 -0500

    Se elimina jwtutils

--- src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/controller/RestaurantRestController.java
+++ src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/controller/RestaurantRestController.java
@@ -1,26 +1,18 @@
 package com.pragma.powerup.usermicroservice.adapters.driving.http.controller;
 
-
-import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.exceptions.RoleNotAllowedForCreationException;
 import com.pragma.powerup.usermicroservice.adapters.driving.http.dto.request.RestaurantRequestDto;
-import com.pragma.powerup.usermicroservice.adapters.driving.http.exceptions.RoleNotAllowedForRestaurantException;
 import com.pragma.powerup.usermicroservice.adapters.driving.http.handlers.IRestaurantHandler;
 import com.pragma.powerup.usermicroservice.configuration.Constants;
-import com.pragma.powerup.usermicroservice.utils.JwtUtils;
-import com.pragma.powerup.usermicroservice.utils.RoleValidationInterceptor;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.interceptor.Interceptors;
 import java.util.Collections;
 import java.util.Map;
 
@@ -31,9 +23,6 @@ public class RestaurantRestController {
 
     private final IRestaurantHandler restaurantHandler;
 
-    @Autowired
-    private final JwtUtils jwtUtils;
-
     @Operation(summary = "Add a new restaurant",
             responses = {
                     @ApiResponse(responseCode = "201", description = "Restaurant created",
@@ -42,10 +31,6 @@ public class RestaurantRestController {
                             content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Error")))})
     @PostMapping("/save")
     public ResponseEntity<Map<String, String>> saveRestaurant(@RequestHeader("Authorization") String jwtToken,@Valid @RequestBody RestaurantRequestDto restaurantRequestDto) {
-        String role = jwtUtils.extractRoleFromJwt(jwtToken);
-        if (!role.equals(Constants.ROLE_ADMIN)){
-            throw new RoleNotAllowedForCreationException();
-        }
         restaurantHandler.saveRestaurant(restaurantRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(Collections.singletonMap(Constants.RESPONSE_MESSAGE_KEY, Constants.PERSON_CREATED_MESSAGE));
